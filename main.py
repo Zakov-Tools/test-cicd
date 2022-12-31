e21d2e54338c13ab04d25e6cabc551e2346be6b3
Iago Gonzalez <6860957+igonro@users.noreply.github.com>
2022-12-31 19:55:54 +0100

    add test website

---
+++ main.py
@@ -0,0 +1,9 @@
+HTML = """<head>
+    <link rel="stylesheet" href="static/css/styles.css">
+</head>
+<body>
+    Hello World!
+    <script src="/static/js/script.js"></script>
+</body>"""
+
+print(HTML)
