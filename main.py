8efe4b6a17ecda950fe60b8076613d6e8aef72cb
Iago Gonzalez <6860957+igonro@users.noreply.github.com>
2022-12-31 20:05:11 +0100

    change static to relative

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 HTML = """<head>
-    <link rel="stylesheet" href="/static/css/styles.css">
+    <link rel="stylesheet" href="static/css/styles.css">
 </head>
 <body>
     Hello World!
-    <script src="/static/js/script.js"></script>
+    <script src="static/js/script.js"></script>
 </body>"""
 
 print(HTML)
